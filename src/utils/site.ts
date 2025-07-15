c0969b96ad808a6286dd6d174b4938e35b78600b
sungsagiyas <sungsagiyas@gmail.com>
2025-07-15 14:04:27 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Jilbab Terupdated';
-export const desk = 'Video Bokep Indo Jepang Jilbob Jav Hijab Live Tante Viral Terbaru Bocil Ngentot Jilbab Smp Skandal';
-export const url = 'https://bokepjilbabterupdated.pages.dev';
-export const terbit = '2025-05-16T00:01:03Z';
